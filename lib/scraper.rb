39b8af65fe13609a1b5b7b034d7f04126b3a1307
Dina Kendle <dina.kendle@gmail.com>
2020-05-05 12:42:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+    student_roster = doc.css(".roster-cards-container")
+
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
-end
 
+end
